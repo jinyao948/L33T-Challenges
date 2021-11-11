9178fa885ef3addf60ec0aed23828bd0dd7255fa
jinyao948 <87969338+jinyao948@users.noreply.github.com>
2021-11-11 16:59:02 +0800

    Update solutions.js

--- solutions.js
+++ solutions.js
@@ -3,6 +3,17 @@
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 // You can return the answer in any order.
+//for loop
+
+var twoSum = (nums, target) => {
+	for(var i=0; i<nums.length; i++ ) {
+		for(var j=i+1; j<nums.length; j++) {
+if(nums[i] + nums[j] === target) {
+return [i,j];
+			}
+		}
+	} 
+}
 
 //normaml solution
 const twoSum = (nums, target) => {
