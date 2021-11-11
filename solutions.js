7582d7d778086ed838f77c1fbcf215e617e2cb40
jinyao948 <87969338+jinyao948@users.noreply.github.com>
2021-11-11 16:27:02 +0800

    Update solutions.js

--- solutions.js
+++ solutions.js
@@ -4,8 +4,25 @@
 
 // You can return the answer in any order.
 
+//normaml solution
+const twoSum = (nums, target) => {
+  const map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const another = target - nums[i];
+
+    if (another in map) {
+      return [map[another], i];
+    }
+
+    map[nums[i]] = i;
+  }
+
+  return null;
+};
 
 
+//hash solution
 const twoSum = function(nums, target) {
     let hashmap = new Map();
     nums.forEach(function(item, i) {
